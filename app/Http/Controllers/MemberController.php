e345dfcb5d05b57bde314148c4e3c40babd27f23
deagus <dedeaskyt121@gmail.com>
2023-12-25 17:35:14 +0800

    testing crud between query builder and Eloquent

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -12,18 +12,51 @@ class MemberController extends Controller
 {
     public function index() {
         try {
-            // Eloquent ORM
+            // Eloquent ORM --get
             // $members = Member::all();
 
-            // Query Builder
-            $members = DB::table('members')->get();
+            // Query Builder --get
+            // $members = DB::table('members')->get();
 
+            // Query builder --insert
+            // DB::table('members')->insert([
+            //    'username' => 'arthur',
+            //    'email' => 'arthur@gmail.com',
+            //    'no_hp' => '08123456789',
+            //    'gender' => 1,
+            //    'alamat' => 'jl. arthur',
+            // ]);
+
+            // Eloquent ORM --insert
+            // Member::create([
+            //     'username' => 'arthur2',
+            //     'email' => 'arthur2@gmail.com',
+            //     'no_hp' => '08123456789',
+            //     'gender' => 1,
+            //     'alamat' => 'jl. arthur',
+            // ]);
+
+            // Query builder --update
+            // DB::table('members')->where('id', 32)->update([
+            //     'gender' => '0'
+            // ]);
+
+            // Eloquent ORM --update
+            // Member::find(32)->update([
+            //     'gender' => '1'
+            // ]);
+
+            // Query builder --delete
+            // DB::table('members')->where('id', 32)->delete();
+
+            // Eloquent ORM --delete
+            // Member::find(31)->delete();
         } catch (ModelNotFoundException $e) {
             return redirect()->route('home')->with('error', 'Data member tidak ditemukan.');
         } catch (QueryException $e) {
             return redirect()->route('home')->with('error', 'Terjadi kesalahan dalam mengambil data member.');
         }
 
-        return view('home', ['members' => $members]);
+        // return view('home', ['members' => $members]);
     }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'username',
+        'email',
+        'no_hp',
+        'gender',
+        'alamat' ,
+    ];
 }
